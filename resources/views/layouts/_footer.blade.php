411a77f1be7876031a4204995d3711db719319a8
chengluck <wangcheng_luck@163.com>
2020-08-14 17:17:10 +0000

    站点设置

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
       由 <a href="http://weibo.com/u/18375153744?is_hot=1" target="_blank">Cheng</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
     </p>
 
-    <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p>
+    <p class="float-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
   </div>
 </footer>

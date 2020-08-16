c06b03b8556696042cda20f4ed183abc6de585b1
chengluck <wangcheng_luck@163.com>
2020-08-16 07:04:34 +0000

    短信调试完成

---
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Overtrue\EasySms\EasySms;
+use App\Http\Requests\Api\VerificationCodeRequest;
+use Overtrue\EasySms\Exceptions\NoGatewayAvailableException;
+
+class VerificationCodesController extends Controller
+{
+    public function store(VerificationCodeRequest $request, EasySms $easySms)
+    {
+        $phone = $request->phone;
+
+        // 生成4位随机数,左侧补0
+        $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+        try{
+            $result = $easySms->send($phone, [
+                'template' => config('easysms.gateways.aliyun.templates.register'),
+                'data' => [
+                    'code' => $code
+                ],
+            ]);
+        }catch(NoGatewayAvailableException $exception){
+            $message = $exception->getException('aliyun')->getMessage();
+            abort(500, $message ?: '短信发送异常');
+        }
+
+        $key = 'verificationCode_' . Str::random(15);
+        $expireAt = now()->addMinutes(5);
+        // 缓存验证码 5 分钟过期
+        \Cache::put($key, ['phone' => $phone, 'code' => $code], $expireAt);
+
+        return response()->json([
+            'key' => $key,
+            'expired_at' => $expireAt->toDateTimeString(),
+        ])->setStatusCode(201);
+    }
+}
